8eb63e270dab98ca819792f5dc3adb13e2432d44
Your Name <you@example.com>
2017-02-12 21:35:53 -0800

    refactor to method builder, added route context to methods

--- thesis.js
+++ thesis.js
@@ -9,42 +9,27 @@ const compilers = require('./lib/compilers');
 function fetchWSDL(wsdl) {
   soap.createClient(wsdl, (err, client) => {
     const description = client.describe();
+    const serviceNames = Object.keys(description);
 
     // ensure main app dir exsits
     if (!fs.existsSync(baseDir)) {
       fs.mkdirSync(baseDir);
     }
-    fs.writeFile(path.join(baseDir, 'app.js'), compilers.app(), (writeErr) => {
-      if (writeErr) {
-        return console.error(err);
-      }
-    });
+
     fs.writeFile(path.join(baseDir, 'package.json'), compilers.package({ appName }), (writeErr) => {
       if (writeErr) {
         return console.error(err);
       }
     });
-    Object.keys(description).forEach((serviceName) => {
-      const preparedServices = serviceBuilder(serviceName, description[serviceName]);
-      const portIndexFileName = path.join(baseDir, serviceName, 'index.js');
-      const portNames = compilers.portIndex({ ports: preparedServices.map(ps => ps.portName) });
 
-      fs.writeFile(portIndexFileName, portNames, (writeErr) => {
-        if (writeErr) {
-          return console.error(err);
-        }
-      });
-      preparedServices.forEach((preparedService) => {
-        const serviceFileText = compilers.methodRoute(preparedService);
-        const portMethodFileName = path.join(
-          baseDir, serviceName, 'methods', `${preparedService.portName}.js`);
-        fs.writeFile(portMethodFileName, serviceFileText, (writeErr) => {
-          if (writeErr) {
-            return console.error(err);
-          }
-        });
-      });
+    fs.writeFile(path.join(baseDir, 'app.js'), compilers.app(serviceNames), (writeErr) => {
+      if (writeErr) {
+        return console.error(err);
+      }
     });
+
+    // builds out all services
+    serviceNames.forEach(sn => serviceBuilder(sn, description[sn]));
   });
 }
 
