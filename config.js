0d3a57afc57c4cf6d17755147b94d0547f33e477
Luc Hall <lucmhall@gmail.com>
2017-02-12 20:39:52 -0800

    improved app location cofigurability and made app runnable

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
-const baseDir = path.join(__dirname, 'app');
+const appName = 'soapy-app';
+const baseDir = path.join(__dirname, '..', appName);
 const wsdl = 'http://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php?wsdl';
 
 module.exports = {
   baseDir,
   wsdl,
+  appName,
 };
