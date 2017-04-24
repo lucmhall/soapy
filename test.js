6816e80e9152d135207051ac554f9ef42c8500c4
swoogie <lucmhall@gmail.com>
2017-04-24 01:17:08 -0700

    Improved xmltojson to handle various child node types

--- test.js
+++ test.js
@@ -2,14 +2,25 @@ const jsonToXML = require('./lib/jsonToXML');
 const XMLToJson = require('./lib/XMLToJson');
 
 
-// const xml = jsonToXML([
-//   { name: 'p', attributes: { fizz: 'buzz' }, children: { name: 'a', attributes: { b: '2' } } },
-//   { name: 'pa', attributes: { foo: 'bar' } },
-// ]);
+const xml = jsonToXML([
+  {
+    name: 'p',
+    attributes: { fizz: 'buzz' },
+    children: {
+      name: 'a',
+      attributes: { b: '2' },
+    },
+  },
+  {
+    name: 'pa',
+    attributes: { foo: 'bar' },
+    children: 'text123',
+  },
+]);
 
-// console.log(xml);
-const json = XMLToJson('<p fizz="buzz"><a b="2">Hello</a></p><pa foo="bar"></pa>');
+console.log(xml);
+const json = XMLToJson(xml);
 
-console.log(json[0].children);
+console.log(json);
 
 
