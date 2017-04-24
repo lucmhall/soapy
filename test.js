7a7b75630837807cb4c5dcc0c0a9eb88f15b8a9c
Your Name <you@example.com>
2017-04-24 00:29:48 -0700

    refactor of xmltojson, childnodes werent working. Still need support for child text nodes

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ const jsonToXML = require('./lib/jsonToXML');
 const XMLToJson = require('./lib/XMLToJson');
 
 
-const xml = jsonToXML([
-  { name: 'p', attributes: { fizz: 'buzz' } },
-  { name: 'pa', attributes: { foo: 'bar' } },
-]);
+// const xml = jsonToXML([
+//   { name: 'p', attributes: { fizz: 'buzz' }, children: { name: 'a', attributes: { b: '2' } } },
+//   { name: 'pa', attributes: { foo: 'bar' } },
+// ]);
 
+// console.log(xml);
+const json = XMLToJson('<p fizz="buzz"><a b="2">Hello</a></p><pa foo="bar"></pa>');
 
-const json = XMLToJson(xml);
-
-console.log(json);
+console.log(json[0].children);
 
 
