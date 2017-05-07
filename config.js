bf9bbff47e26fe6e929303cebc369c742d529b31
swoogie <lucmhall@gmail.com>
2017-05-06 17:50:01 -0700

    added watch tasks, fixed but not including all operation types, added check before parsing json -> xml

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const appName = 'soapy-app';
 const baseDir = path.join(__dirname, '..', appName);
-const wsdl = 'http://www.webservicex.net/usaddressverification.asmx?WSDL';
+const wsdl = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php?wsdl';
 
 module.exports = {
   baseDir,
